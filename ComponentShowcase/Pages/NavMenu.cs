3741801ec64cec45276a32362640df6d5245facc
Lachezar Georgiev <georgiev.lb@gmail.com>
2020-09-06 18:34:59 +0300

    Cleanup

--- ComponentShowcase/Pages/NavMenu.cs
+++ ComponentShowcase/Pages/NavMenu.cs
@@ -1,47 +1,15 @@
 ﻿using ComponentShowcase.Models;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ComponentShowcase.Pages
 {
     public partial class NavMenu : ComponentBase
     {
         [Parameter]
-        public IEnumerable<NavItem> RootItem { get; set; }
+        public IEnumerable<NavItem> Items { get; set; }
 
-        private void InitializeRootItems()
-        {
-            RootItem = new List<NavItem>()
-            {
-                new NavItem
-                {
-                    Text = "Item 1", Children = new List<NavItem>()
-                    {
-                        new NavItem { Text = "Item 1.1" },
-                        new NavItem { Text = "Item 1.2", Children = new List<NavItem>
-                        {
-                            new NavItem {Text = "Item 1.2.1" }, new NavItem { Text = "Item 1.2.2"} }
-                        },
-                        new NavItem { Text = "Item 1.3", Children = new List<NavItem>
-                        {
-                            new NavItem { Text = "Item 1.3.1", Children = new List<NavItem>(){ new NavItem() { Text = ""} } }
-                        }
-                        }
-                    }
-                },
-                new NavItem
-                {
-                    Text = "Item 2",
-                },
-                new NavItem
-                {
-                    Text = "Item 3"
-                }
-            };
-        }
-
-        public static IEnumerable<NavItem> ReadyItems = new List<NavItem>()
+        public static IEnumerable<NavItem> TestItems = new List<NavItem>()
             {
                 new NavItem
                 {
@@ -83,12 +51,12 @@ namespace ComponentShowcase.Pages
                                 new NavItem
                                 {
                                     Text = "Item 1.3.1"
-                                    //, Children = new List<NavItem>()
-                                    //{
-                                    //    new NavItem()
-                                    //{
-                                    //    Text = "Item 1.3.1.1"}
-                                    //}
+                                    , Children = new List<NavItem>()
+                                    {
+                                        new NavItem()
+                                    {
+                                        Text = "Item 1.3.1.1"}
+                                    }
                                 }
                             }
                         }
@@ -100,14 +68,20 @@ namespace ComponentShowcase.Pages
                 },
                 new NavItem
                 {
-                    Text = "Item 3"
+                    Text = "Item 3", Children = new List<NavItem>
+                            {
+                                new NavItem
+                                {
+                                    Text = "Item 3.1"
+                                    , Children = new List<NavItem>()
+                                    {
+                                        new NavItem()
+                                    {
+                                        Text = "Item 3.1.1"}
+                                    }
+                                }
+                            }
                 }
             };
-
-        protected override Task OnInitializedAsync()
-        {
-            //InitializeRootItems();
-            return base.OnInitializedAsync();
-        }
     }
 }
